ff237b9cd8f8c534b987670c19ec03d6b2f464b7
NiBa97 <niba97@icloud.com>
2025-02-06 18:34:20 +0100

    Initial Commit

---
+++ pocketbase-realtime-watchos Watch App/pocketbase_realtime_watchosApp.swift
@@ -0,0 +1,17 @@
+//
+//  pocketbase_realtime_watchosApp.swift
+//  pocketbase-realtime-watchos Watch App
+//
+//  Created by Niklas Bauer on 06.02.25.
+//
+
+import SwiftUI
+
+@main
+struct pocketbase_realtime_watchos_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
